da807f599300b06a356c30f86d669fb55cb335c5
Circlemak <Circlemak0605@gmail.com>
2025-03-05 14:29:05 +0800

    trying to turn json into list

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,7 @@ from mc_rcon import RCONClient
 from system_monitor import SystemMonitor
 from log_parser import LogParser
 from threading import Lock
+import json
 
 print(SystemMonitor.get_stats())
 app = Flask(__name__)
@@ -21,6 +22,7 @@ def players():
 
 @app.route('/api/system_stats', methods=['GET'])
 def system_stats():
+    print(json.loads(SystemMonitor.get_stats()))
     return SystemMonitor.get_stats()
 
     
