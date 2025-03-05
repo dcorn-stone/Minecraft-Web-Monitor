de12352ec595bec48415c429d084df878dc9cdc2
corn <gayi_chen@outlook.com>
2025-03-05 14:33:12 +0800

    fixing app.py

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,6 @@ from mc_rcon import RCONClient
 from system_monitor import SystemMonitor
 from log_parser import LogParser
 from threading import Lock
-import json
 
 print(SystemMonitor.get_stats())
 app = Flask(__name__)
@@ -21,7 +20,6 @@ def players():
 
 @app.route('/api/system_stats', methods=['GET'])
 def system_stats():
-    print(json.loads(SystemMonitor.get_stats()))
     return SystemMonitor.get_stats()
 
     
